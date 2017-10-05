c5809a2a5efa456f1fbe162b249c5244d91c9edb
Yang Yuqing <yangyuqinghhh@gmail.com>
2017-10-05 15:39:20 +0800

    Add files via upload

---
+++ database/loan_log.sql
@@ -0,0 +1,21 @@
+INSERT INTO `loan_log`(`stuff`, `borrower`, `loan_date`, `return_date`, `price`) VALUES (4, 1, "2018-01-20", "2018-02-27", 100);
+INSERT INTO `loan_log`(`stuff`, `borrower`, `loan_date`, `return_date`, `price`) VALUES (5, 12, "2018-01-11", "2018-04-12", 12.4);
+INSERT INTO `loan_log`(`stuff`, `borrower`, `loan_date`, `return_date`, `price`) VALUES (2, 17, "2018-04-28", "2018-12-06", 22.3);
+INSERT INTO `loan_log`(`stuff`, `borrower`, `loan_date`, `return_date`, `price`) VALUES (10, 6, "2018-04-08", "2018-02-27", 6.6);
+INSERT INTO `loan_log`(`stuff`, `borrower`, `loan_date`, `return_date`, `price`) VALUES (17, 7, "2018-01-01", "2018-01-02", 91.3);
+INSERT INTO `loan_log`(`stuff`, `borrower`, `loan_date`, `return_date`, `price`) VALUES (77, 11, "2018-01-11", "2018-01-27", 2);
+INSERT INTO `loan_log`(`stuff`, `borrower`, `loan_date`, `return_date`, `price`) VALUES (15, 17, "2018-01-20", "2018-02-16", 17.5);
+INSERT INTO `loan_log`(`stuff`, `borrower`, `loan_date`, `return_date`, `price`) VALUES (22, 10, "2018-02-04", "2018-04-27", 20);
+INSERT INTO `loan_log`(`stuff`, `borrower`, `loan_date`, `return_date`, `price`) VALUES (14, 2, "2018-03-14", "2018-05-16", 30);
+INSERT INTO `loan_log`(`stuff`, `borrower`, `loan_date`, `return_date`, `price`) VALUES (3, 19, "2018-06-07", "2018-07-07", 31.2);
+INSERT INTO `loan_log`(`stuff`, `borrower`, `loan_date`, `return_date`, `price`) VALUES (55, 20, "2018-01-17", "2018-02-27", 100);
+INSERT INTO `loan_log`(`stuff`, `borrower`, `loan_date`, `return_date`, `price`) VALUES (13, 4, "2018-02-05", "2018-02-07", 22.2);
+INSERT INTO `loan_log`(`stuff`, `borrower`, `loan_date`, `return_date`, `price`) VALUES (23, 5, "2018-03-20", "2018-04-27", 7.5);
+INSERT INTO `loan_log`(`stuff`, `borrower`, `loan_date`, `return_date`, `price`) VALUES (25, 10, "2018-05-07", "2018-07-13", 34);
+INSERT INTO `loan_log`(`stuff`, `borrower`, `loan_date`, `return_date`, `price`) VALUES (7, 16, "2018-04-28", "2018-12-26", 45.2);
+INSERT INTO `loan_log`(`stuff`, `borrower`, `loan_date`, `return_date`, `price`) VALUES (26, 14, "2018-08-01", "2018-09-13", 10);
+INSERT INTO `loan_log`(`stuff`, `borrower`, `loan_date`, `return_date`, `price`) VALUES (21, 2, "2018-09-12", "2018-09-13", 12);
+INSERT INTO `loan_log`(`stuff`, `borrower`, `loan_date`, `return_date`, `price`) VALUES (19, 9, "2018-01-14", "2018-02-22", 5);
+INSERT INTO `loan_log`(`stuff`, `borrower`, `loan_date`, `return_date`, `price`) VALUES (45, 23, "2018-10-21", "2018-10-23", 34.2);
+INSERT INTO `loan_log`(`stuff`, `borrower`, `loan_date`, `return_date`, `price`) VALUES (24, 15, "2018-07-15", "2018-12-09", 10.5);
+
