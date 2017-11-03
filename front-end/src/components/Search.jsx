962b27879d2c9fad9c00e74a9df5a51e36c4b252
riwu <riwu0730@gmail.com>
2017-11-03 15:59:18 +0800

    styling controls

--- front-end/src/components/Search.jsx
+++ front-end/src/components/Search.jsx
@@ -15,31 +15,17 @@ import './Search.css';
 
 const Search = ({ search, setFilter }) => (
   <div className="Search">
-    <FormGroup>
+    <Form inline className="search">
       <FormControl
+        className="formGroup"
         type="text"
         placeholder="Search"
         value={search.name}
         onChange={e => setFilter('name', e.target.value)}
       />
-    </FormGroup>
-    {' '}
-    <Form inline>
-      <ControlLabel>Show</ControlLabel>
-      {' '}
-      <FormControl
-        type="number"
-        value={search.count}
-        min={0}
-        max={500}
-        step={50}
-        onChange={e => setFilter('count', e.target.value)}
-      />
-      {' '}
-      <ControlLabel>per page</ControlLabel>
     </Form>
 
-    <Form inline>
+    <Form className="formGroup">
       <ControlLabel>Category</ControlLabel>
       {' '}
       <DropdownButton
@@ -58,52 +44,18 @@ const Search = ({ search, setFilter }) => (
       </DropdownButton>
     </Form>
 
-    <div className="filter">
-      <ControlLabel>Price range</ControlLabel>
-      <Range
-        value={search.price}
-        marks={{
-          0: 'FREE',
-          100: 'MAX',
-          ...[...Array(9).keys()].reduce((obj, value) => {
-            const key = (value + 1) * 10;
-            obj[key] = key; //eslint-disable-line
-            return obj;
-          }, {}),
-        }}
-        step={5}
-        onChange={value => setFilter('price', value)}
-      />
-    </div>
-
-    <Form inline className="filter">
-      <ControlLabel>Condition</ControlLabel>
-      <Range
-        value={search.condition}
-        min={1}
-        max={5}
-        marks={{
-          ...[...Array(5).keys()].reduce((obj, value) => {
-            const key = value + 1;
-            obj[key] = key; //eslint-disable-line
-            return obj;
-          }, {}),
-        }}
-        step={1}
-        onChange={value => setFilter('condition', value)}
+    <Form className="formGroup">
+      <ControlLabel>Available before</ControlLabel>
+      {' '}
+      <DayPickerInput
+        value={search.availableDate}
+        format="D MMM YY"
+        onDayChange={day => setFilter('availableDate', day.format('D MMM YY'))}
       />
     </Form>
 
-    <CountrySelect
-      multi
-      flagImagePath="./assets/flags/"
-      value={search.location}
-      onSelect={countries => setFilter('location', countries)}
-    />
-
-
     <Form inline>
-      <ControlLabel>Minimum loan period</ControlLabel>
+      <ControlLabel>Minimum loan period:</ControlLabel>
       {' '}
       <FormControl
         type="number"
@@ -117,26 +69,66 @@ const Search = ({ search, setFilter }) => (
       <ControlLabel>days</ControlLabel>
     </Form>
 
-    <Form inline>
+    <div className="filter">
+      <ControlLabel>Price range</ControlLabel>
+      <Range
+        className="slider"
+        value={search.price}
+        marks={{
+          0: 'FREE',
+          100: 'MAX',
+          ...[...Array(9).keys()].reduce((obj, value) => {
+            const key = (value + 1) * 10;
+            obj[key] = '$' + key; //eslint-disable-line
+            return obj;
+          }, {}),
+        }}
+        step={5}
+        onChange={value => setFilter('price', value)}
+      />
+    </div>
+
+    <Form className="filter">
+      <ControlLabel>Condition</ControlLabel>
+      <Range
+        className="slider"
+        value={search.condition}
+        min={1}
+        max={5}
+        marks={{
+          1: 'Very old',
+          5: 'New',
+          ...[...Array(3).keys()].reduce((obj, value) => {
+            const key = value + 2;
+            obj[key] = key; //eslint-disable-line
+            return obj;
+          }, {}),
+        }}
+        step={1}
+        onChange={value => setFilter('condition', value)}
+      />
+    </Form>
+
+    <Form inline className="formGroup">
       <ControlLabel>Owner</ControlLabel>
       {' '}
       <FormControl
         type="text"
         placeholder="Enter owner name"
-        value={search.name}
-        onChange={e => setFilter('name', e.target.value)}
+        value={search.owner}
+        onChange={e => setFilter('owner', e.target.value)}
       />
     </Form>
-
-
-    <Form inline>
-      <ControlLabel>Available on</ControlLabel>
-      {' '}
-      <DayPickerInput
-        value={search.availableDate}
-        format="D MMM YY"
-        onDayChange={day => setFilter('availableDate', day.format('D MMM YY'))}
-      />
+    <Form inline className="countryContainer">
+      <ControlLabel className="controlLabel">Country</ControlLabel>
+      <div className="country">
+        <CountrySelect
+          multi
+          flagImagePath="./assets/flags/"
+          value={search.location}
+          onSelect={countries => setFilter('location', countries)}
+        />
+      </div>
     </Form>
 
     <Form inline>
@@ -152,18 +144,33 @@ const Search = ({ search, setFilter }) => (
             onClick={() => setFilter('sort', category)}
             active={search.sort === category}
           >{category}</MenuItem>
-      ))}
+        ))}
       </DropdownButton>
       {' '}
-      Asc
-      <Switch
-        checked={search.asc}
-        onChange={value => setFilter('asc', value)}
+        Asc
+        <Switch
+          checked={search.asc}
+          onChange={value => setFilter('asc', value)}
+        />
+        Desc
+      </Form>
+
+    <Form inline>
+      <ControlLabel>Show</ControlLabel>
+      {' '}
+      <FormControl
+        type="number"
+        value={search.count}
+        min={0}
+        max={500}
+        step={50}
+        onChange={e => setFilter('count', e.target.value)}
       />
-      Desc
+      {' '}
+      <ControlLabel>per page</ControlLabel>
     </Form>
 
-    <Button type="submit">Submit</Button>
+    <Button bsStyle="primary">Submit</Button>
   </div>
 );
 
