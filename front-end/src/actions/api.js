dd5379e301fe80552ccbfad45ca3a05d9131f0c8
riwu <riwu0730@gmail.com>
2017-11-03 19:03:02 +0800

    update search front-end api

--- front-end/src/actions/api.js
+++ front-end/src/actions/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import moment from 'moment';
 
 axios.defaults.baseURL = 'http://localhost:3001/';
 
@@ -14,8 +15,27 @@ const [post, patch, put] = ['post', 'patch', 'put'].map(method =>
     throw new Error((err.response || {}).data);
   }));
 
+
 export default {
-  getStuffs: search => get('stuff'),
+  getStuffs: (search) => {
+    const query = {
+      ...Object.entries(search)
+        .filter(([key, value]) => ['name', 'count', 'page',
+          'sort', 'name', 'asc', 'maxLoan', 'owner'].includes(key))
+        .reduce((obj, [key, value]) => {
+          obj[key] = value;
+          return obj;
+        }, {}),
+      availableDate: moment(search.availableDate, 'D MMM YY').format('YYYY-MM-DD'),
+      category: search.category === 'All' ? undefined : search.category,
+      priceLow: search.price[0],
+      priceHigh: search.price[1],
+      conditionLow: search.condition[0],
+      conditionHigh: search.condition[1],
+      location: search.location.join(),
+    };
+    return get(`stuff?${Object.entries(query).reduce((str, [key, value]) => `${str + key}=${encodeURIComponent(value)}&`, '')}`);
+  },
   getUsers: () => get('users'),
   getLoans: () => get('bids'),
   getBids: () => get('loans'),
