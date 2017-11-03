820fe28957fdc75cf995e65acec93c9e4c2872c6
Snigdha <singhaniasnigdha@gmail.com>
2017-11-03 15:57:32 +0800

    working on owner profile - me.js

--- back-end/routes/me.js
+++ back-end/routes/me.js
@@ -12,8 +12,18 @@ router.get('/:username', (req, res, next) => {
 });
 
 router.get('/:username/:stuffId', (req, res, next) => {
-	const response = conn.query(queries.allSafeUserData);
+	const response = conn.query(queries.getBidsFor(username, stuffId));
 	return response.then(data => res.send(data));
 });
+
+router.post('/bidSelect', (req, res, next) => {
+	var bidWin = {'user': req.body.username, 'stuffId': req.body.stuffId, 'bidAmt': req.body.bidAmt};
+	const response = conn.query(queries.setBidWinner(bidWin));
+	return response.then((data) => {
+		conn.query.borrowStuff(bidWin);
+	}).then(data => res.send(data));
+});
+
+
 
 module.exports = router;
