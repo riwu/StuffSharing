f1af7cfa0a933821fa1c2cf826696cb41d1bd17c
riwu <riwu0730@gmail.com>
2017-11-04 15:14:46 +0800

    fix register on backend

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -6,32 +6,33 @@ const queries = require('./queries');
 const router = express.Router();
 
 router.post('/login', (req, res, next) => {
-	const login = {'username': req.body.username, 'password': req.body.password};
-	const response = conn.query(queries.loginUser(login));
-	return response.then(data => {
-		console.log("Login Data: " + data);
-		if (data && data.length > 0) {
-			res.end();
-		} else {
-			res.status(500);
-		}
+  const login = { username: req.body.username, password: req.body.password };
+  const response = queries.loginUser(login);
+  return response.then((data) => {
+    console.log(`Login Data: ${data}`);
+    if (data && data.length > 0) {
+      res.end();
+    } else {
+      res.status(500);
+    }
 	 });
 });
 
 router.post('/register', (req, res, next) => {
-	const register = {'username': req.body.username, 'password': req.body.password,
-						'email': req.body.email, 'firstName': req.body.firstName,
-						'lastName': req.body.lastName};
-	const response = conn.query(queries.checkUsername(register.username));
-	return response.then(data => {
-		if (data && data.length > 0) {
-			res.status(500);
-		} else {
-			const response2 = conn.query(queries.registerUser(register));
-			response2.then(res.end());
-			return;
-		}
-	});
+  const register = { username: req.body.username,
+    password: req.body.password,
+    email: req.body.email,
+    first_name: req.body.firstName,
+    last_name: req.body.lastName };
+  const response = queries.checkUsername(register);
+  return response.then((data) => {
+    if (data && data.length > 0) {
+      res.status(500);
+    } else {
+      const response2 = queries.registerUser(register);
+      response2.then(res.end());
+    }
+  });
 });
 
 router.get('/', (req, res, next) =>
@@ -46,7 +47,6 @@ router.get('/', (req, res, next) =>
 // 	// update my name password or email
 // 	// cannot change username
 // });
-
 
 
 router.get('/loans', (req, res, next) => {
