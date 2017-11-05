e07d90c1b5cafa207d78768037fad08f386e91a5
riwu <riwu0730@gmail.com>
2017-11-06 00:50:26 +0800

    fix backend compile error

--- back-end/routes/me.js
+++ back-end/routes/me.js
@@ -10,26 +10,29 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
 	// Get all the bids on my products
-	const response = conn.query(bid.getAllMyBids(username));
-	return response.then(data => res.send(data));
+  const response = conn.query(bid.getAllMyBids(username));
+  return response.then(data => res.send(data));
 });
 
 router.get('/:username/:stuffId', (req, res, next) => {
-	const response = conn.query(bid.getBidsFor(username, stuffId));
-	return response.then(data => res.send(data));
+  const response = conn.query(bid.getBidsFor(username, stuffId));
+  return response.then(data => res.send(data));
 });
 
 router.post('/bidSelect', (req, res, next) => {
-	var bidWin = {'bidder': req.body.username, 'stuffId': req.body.stuffId,
-					'bidAmt': req.body.bidAmt, 'loanDate': req.body.loanDate};
-	const response = conn.query(loan.addLoanLog(bidWin));
-	return response.then((data) => {
-		const response2 = stuff.updateStuffLoan(bidWin.stuffId, bidWin.loanDate);
-		return response2.then(data2 => res.send(data2));
+  const bidWin = { bidder: req.body.username,
+    stuffId: req.body.stuffId,
+    bidAmt: req.body.bidAmt,
+    loanDate: req.body.loanDate };
+  const response = conn.query(loan.addLoanLog(bidWin));
+  return response.then((data) => {
+    const response2 = stuff.updateStuffLoan(bidWin.stuffId, bidWin.loanDate);
+    return response2.then(data2 => res.send(data2));
+  });
 });
 
 router.get('/update', (req, res, next) => {
-	res.send('Update User details');
+  res.send('Update User details');
 });
 
 module.exports = router;
