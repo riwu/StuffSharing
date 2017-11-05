cc319433f86882f55d7a7240446b26b1a0579994
Snigdha <singhaniasnigdha@gmail.com>
2017-11-05 20:21:32 +0800

    user can add and delete stuff

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -6,6 +6,23 @@ const queries = require('./queries');
 
 const router = express.Router();
 
+router.post('/add/stuff', (req, res, next) => {
+	// Add new stuff
+	var stuffInfo = {'name': req.body.name, 'desc': req.body.desc, 'condition': req.body.condition, 'category': category,
+					  	'location': req.body.location, 'owner': req.body.owner, 'price': req.body.price,
+					  	'available_from': req.body.available_from, 'max_loan_period': req.body.max_loan_period};
+
+	const response = conn.query(queries.addStuff(stuffInfo));
+	return response.then(data => res.send(data));
+});
+
+router.post('/stuff/delete', (req, res, next) => {
+	// Delete this stuff
+	const response = conn.query(queries.deleteStuff(req.body.stuffId));
+	return response.then(data => res.send(data));
+});
+
+
 /* GET users listing. */
 router.get('/', (req, res, next) => {
   const response = conn.query(queries.allSafeUserData);
