10183ac0d58ded3c2f1d016caf3513c2b185b87c
riwu <riwu0730@gmail.com>
2017-11-11 19:43:04 +0800

    add borrower field

--- front-end/src/components/StuffsLent.jsx
+++ front-end/src/components/StuffsLent.jsx
@@ -5,8 +5,12 @@ import moment from 'moment';
 import { stuffReturned } from '../actions';
 import Stuffs from './Stuffs';
 
-const StuffLoanDate = ({ stuff }) => (
+const LoanDate = ({ stuff }) => (
   <td>{moment(stuff.loan_date).format('D MMM YY')}</td>
+);
+
+const Borrower = ({ stuff }) => (
+  <td>{stuff.borrower}</td>
 );
 
 const StuffChildren = ({ stuff, ...props }) => (
@@ -27,8 +31,8 @@ const StuffChildrenConnected = connect(null, {
 const StuffsListed = props => (
   <Stuffs
     stuffs={props.stuffs}
-    extraHeaders={['Loan date']}
-    extra={[StuffLoanDate, StuffChildrenConnected]}
+    extraHeaders={['Loan date', 'Borrower']}
+    extra={[LoanDate, Borrower, StuffChildrenConnected]}
   />
   );
 
