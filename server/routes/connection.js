91fb15cf47b138a9e250857f601a650b13e8d4b6
riwu <me@wangriwu.com>
2018-11-03 10:51:12 +0800

    switch to connection pool

--- server/routes/connection.js
+++ server/routes/connection.js
@@ -1,17 +1,11 @@
 const mysql = require('promise-mysql');
 
-let conn;
-mysql
-  .createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    database: 'stuff_sharing',
-    port: process.env.MYSQL_PORT,
-    password: process.env.MYSQL_PASSWORD,
-  })
-  .then((connection) => {
-    console.log('setting connection');
-    conn = connection;
-  });
+const connection = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  database: 'stuff_sharing',
+  port: process.env.MYSQL_PORT,
+  password: process.env.MYSQL_PASSWORD
+});
 
-export { conn as default };
+export default connection;
